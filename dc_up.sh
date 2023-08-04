0660bf115e9c5d375fa5e64d745659f81ea64263
ritwika <ritwika21296@gmail.com>
2023-08-04 23:56:32 +0530

    up script

--- dc_up.sh
+++ dc_up.sh
@@ -1,3 +1,3 @@
 echo HOST_ADDR=$(curl -s https://api.ipify.org) > .env && \
-docker-compose up -d && \
-docker-compose exec -d dockovpn wget -O /doc/Dockovpn/client.ovpn localhost:8080
+docker compose up -d && \
+docker compose exec -d dockovpn wget -O /doc/Dockovpn/client.ovpn localhost:8080
